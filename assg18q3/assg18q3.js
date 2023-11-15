d52c4238e2763de4f215f195f75cef4a99672f56
Joyrup Singh <145583757+Joyrup-singh@users.noreply.github.com>
2023-11-15 12:12:20 +0530

    Add files via upload

---
+++ assg18q3/assg18q3.js
@@ -0,0 +1,47 @@
+//Use a nested ternary operator to check that a number is positive, negative or zero. You have to print
+// “positive” if the number is positive and similarly for negative and zero also.
+
+
+// program that checks if the number is positive, negative or zero
+
+// If the number is zero
+
+var num = 0;
+
+if(num > 0){
+  console.log("The number is positive");
+}
+else if(num < 0){
+  console.log("The number is negative");
+}
+else{
+  console.log("zero");
+}
+
+// If the number is positive
+
+var num = 6;
+
+if(num > 0){
+  console.log("The number is positive");
+}
+else if(num < 0){
+  console.log("The number is negative");
+}
+else{
+  console.log("zero");
+}
+
+// If the number is negative
+
+var num = -7;
+
+if(num > 0){
+  console.log("The number is positive");
+}
+else if(num < 0){
+  console.log("The number is negative");
+}
+else{
+  console.log("zero");
+}
---
+++ assg18q4/assg18q4.js
@@ -0,0 +1,26 @@
+/*
+Use a ternary operator to check that a person is eligible to vote or not by checking his age. If the age 
+of the the person is less than 18 then “You cannot vote” should be logged else “You can vote” should be logged.
+*/
+
+// Person less than 18yrs
+
+var age = 17;
+
+if( age < 18 ){
+    console.log("You cannot vote");
+}
+else{
+    console.log("You can vote");
+}
+
+// Person more than !8yrs
+
+var age = 20;
+
+if( age < 18 ){
+    console.log("You cannot vote");
+}
+else{
+    console.log("You can vote");
+}
